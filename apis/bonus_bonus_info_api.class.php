d45b8615e8e5f172a2ba37bd82e7c623056354e4
陈哲俊 <huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-04-22 15:45:08 +0800

    购物流相关api修改提交

---
+++ apis/bonus_bonus_info_api.class.php
@@ -0,0 +1,44 @@
+<?php
+defined('IN_ECJIA') or exit('No permission resources.');
+/**
+ * 红包信息
+ * @author will.chen
+ *
+ */
+class bonus_bonus_info_api extends Component_Event_Api {
+    
+    public function call(&$options) {
+        if (!is_array($options) || !isset($options['bonus_id'])) {
+            return new ecjia_error('invalid_parameter', '参数无效');
+        }
+        $options['bonus_sn'] = isset($options['bonus_sn']) ? $options['bonus_sn'] : '';
+        return $this->bonus_info($options['bonus_id'], $options['bonus_sn']);
+    }
+    
+    /**
+	* 取得红包信息
+	* @param   int	 $bonus_id   红包id
+	* @param   string  $bonus_sn   红包序列号
+	* @param   array   红包信息
+	*/
+	private function bonus_info($bonus_id, $bonus_sn = '') 
+	{
+		$dbview	= RC_Loader::load_app_model('user_bonus_type_viewmodel', 'bonus');
+		$dbview->view = array(
+			'bonus_type' => array(
+				'type'	=> Component_Model_View::TYPE_LEFT_JOIN,
+				'alias'	=> 'bt',
+				'field'	=> 'bt.*, ub.*',
+				'on'	=> 'bt.type_id = ub.bonus_type_id'
+			)
+		);
+	
+		if ($bonus_id > 0) {
+			return $dbview->find(array('ub.bonus_id' => $bonus_id));
+		} else {
+			return $dbview->find(array('ub.bonus_sn' => $bonus_sn));
+		}
+	}
+}
+
+// end
