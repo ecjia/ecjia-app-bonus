f5683e81e872ea410f1d895afc17d3437a510df7
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-01 10:55:17 +0800

    平台后台更新红包权限

--- classes/hooks/admin_bonus.class.php
+++ classes/hooks/admin_bonus.class.php
@@ -4,7 +4,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
 class bonus_admin_plugin {
 	
 	static public function bonus_admin_menu_api($menus) {
-	    $menu = ecjia_admin::make_admin_menu('02_bonustype_list', RC_Lang::get('bonus::bonus.bonus_manage'), RC_Uri::url('bonus/admin/init'), 2)->add_purview('bonus_manage');
+	    $menu = ecjia_admin::make_admin_menu('02_bonustype_list', RC_Lang::get('bonus::bonus.bonus_manage'), RC_Uri::url('bonus/admin/init'), 2)->add_purview('bonus_type_manage');
 	    $menus->add_submenu($menu);
 	    return $menus;
 	}
