5974f07b550744e42f9d76533604755c5817f5bc
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-09-17 10:29:23 +0800

    bonus红包模块接口修改

--- modules/bonus/receive_coupon_module.class.php
+++ modules/bonus/receive_coupon_module.class.php
@@ -50,7 +50,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
  * 领取优惠券
  * @author will.chen
  */
-class receive_coupon_module extends api_front implements api_interface {
+class bonus_receive_coupon_module extends api_front implements api_interface {
 	
 	 public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {	
 		$this->authSession();	
